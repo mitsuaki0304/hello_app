c1cda24afa1cc68a5ecd893d43de9b100c1ac29e
mitsuaki0304 <mitsuaki0304@gmail.com>
2020-02-15 14:59:33 +0900

    aaaa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello,World!"
+    render html: "hellod,World!"
   end
 end
